f0689618dbf614a421e05b027283e83ee758d0c0
minnnnnnnnnnn <seongmin2053@naver.com>
2025-05-09 00:40:32 +0900

    스프링 @Configuration @Bean 추가

--- src/main/java/com/example/demo/App02HelloWorldSpring.java
+++ src/main/java/com/example/demo/App02HelloWorldSpring.java
@@ -1,9 +1,32 @@
 package com.example.demo;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class App02HelloWorldSpring {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// 1. Spring Context 시작 - 
+		// Spring Context에서 Bean을 관리
+		
+		var context = 
+				new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
+		
+		// 2. Spring Context가 관리할 것
+		// HelloWorldConfiguration - @Configuration
+		// name - @Bean
+		
+		
+		// 3. Spring이 관리하는 Bean 검색
+		System.out.println(context.getBean("name"));
+		
+		System.out.println(context.getBean("age"));
+		
+		System.out.println(context.getBean("person"));
+		
+		System.out.println(context.getBean("address"));
+
+
 
 	}
 
--- src/main/java/com/example/demo/HelloWorldConfiguration.java
+++ src/main/java/com/example/demo/HelloWorldConfiguration.java
@@ -1,8 +1,34 @@
 package com.example.demo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+// record = 게터 세터 생성자 없이 클래스 생성 가능
+record Person (String name, int age) { };
+record Address (String firstLine, String city) { };
 
 @Configuration
 public class HelloWorldConfiguration {
+	
+	
+	@Bean
+	public String name() {
+		return "Ranga";
+	}
+	
+	@Bean
+	public int age() {
+		return 25;
+	}
 
+	@Bean
+	public Person person() {
+		return new Person("Ravi", 24);
+	}
+	
+	@Bean
+	public Address address() {
+		return new Address("busan", "gaya");
+	}
+	
 }
